6f19b8d5b03220ac0a2c7ae261217c989f9c2810
Ziang <ziangw2@illinois.edu>
2020-11-12 18:27:21 -0800

    Restore the old scale application.

--- benchmark/scale/scale_middle.c
+++ benchmark/scale/scale_middle.c
@@ -20,8 +20,8 @@
 
 
 // The main function: scale a image.
-void scale(uint8_t *image, int width, int height, int channel,
-    uint8_t *scaleImage, float scaleRatio) {
+void scale(uint8_t *image, const int width, const int height, const int channel,
+    uint8_t *scaleImage, const float scaleRatio) {
   const int scaleWidth = width * scaleRatio;
   const int scaleHeight = height * scaleRatio;
 
@@ -41,13 +41,31 @@ void scale(uint8_t *image, int width, int height, int channel,
 
 
 int main(int argc, char* argv[]) {
-  const char* inputPath = "/home/user/Desktop/CS523/gem5/benchmark/scale/img.png";
-  const char* outputPath = "/home/user/Desktop/CS523/gem5/benchmark/scale/out.png";
-  const float scaleRatio = 0.5;
+  if (argc != 7) {
+    fprintf(stderr, "Usage: ./scale input output width height channel scaleRatio\n");
+    return -1;
+  }
+
+  FILE* input = fopen(argv[1], "r");
 
   // Determine the scale and load the image.
   int width, height, channel;
-  uint8_t *image = stbi_load(inputPath, &width, &height, &channel, 0);
+  width = atoi(argv[3]);
+  height = atoi(argv[4]);
+  channel = atoi(argv[5]);
+  uint8_t *image = malloc(width * height * channel * sizeof(uint8_t));
+  int counter = 0;
+  unsigned val[1];
+  int ret = fscanf(input, "%u\n", val);
+  image[counter++] = val[0];
+  while (ret != EOF) {
+    ret = fscanf(input, "%u\n", val);
+    image[counter++] = val[0];
+  }
+  fclose(input);
+
+
+  const float scaleRatio = strtof(argv[6], NULL);
 
   // Allocate the memory for scaled image.
   const int scaleWidth = width * scaleRatio;
@@ -56,11 +74,16 @@ int main(int argc, char* argv[]) {
 
   scale(image, width, height, channel, scaleImage, scaleRatio);
 
-  // Write the output file.
-  stbi_write_png(outputPath, scaleWidth, scaleHeight, channel, scaleImage, scaleWidth * channel);
+  // Write output file
+  FILE* output = fopen(argv[2], "w+");
+  for (int i = 0; i < scaleWidth * scaleHeight * channel; i++) {
+    fprintf(output, "%u\n", scaleImage[i]);
+  }
+
+  fclose(output);
 
   // Free resources.
-  stbi_image_free(image);
   free(scaleImage);
+  free(image);
   return 0;
 }
